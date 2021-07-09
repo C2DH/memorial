e43637f5236fc3704179afa9feffa2a9c9822d15
Daniele Guido <gui.daniele@gmail.com>
2021-07-09 15:05:15 +0200

    Update Person.js

--- src/models/Person.js
+++ src/models/Person.js
@@ -1,5 +1,6 @@
 export default class Person {
   constructor({
+    slug = '',
     firstName = '',
     lastName = '',
     professions = [],
@@ -7,8 +8,10 @@ export default class Person {
     birthPlace = '',
     birthYear = 0,
     notes = '',
-    convoy=''
+    convoy='',
+    households=[],
   }) {
+    this.slug = slug
     this.firstName = firstName
     this.lastName = lastName
     this.professions = professions
@@ -17,5 +20,23 @@ export default class Person {
     this.birthYear = birthYear
     this.convoy = convoy
     this.notes = notes
+    this.households = households
+  }
+
+  static create({ slug, data }){
+    return new Person({
+      slug,
+      firstName: data.first_name,
+      lastName: data.last_name,
+      professions: data.professions,
+      birthDate: data.birth_date,
+      birthPlace: data.birth_place,
+      deathDate: data.death_date,
+      deathPlace: data.death_place,
+      convoy: data.convoy,
+      notes: data.notes,
+      households: data.households,
+      picture: data.resolutions?.thumbnail
+    })
   }
 }
