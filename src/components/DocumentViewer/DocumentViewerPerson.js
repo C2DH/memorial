992a7c05059dbf9e31bbc67f89aa16c808eafd81
Daniele Guido <gui.daniele@gmail.com>
2021-07-09 15:05:32 +0200

    fix document viewer

--- src/components/DocumentViewer/DocumentViewerPerson.js
+++ src/components/DocumentViewer/DocumentViewerPerson.js
@@ -3,38 +3,30 @@ import Person from '../../models/Person'
 import LangLink from '../LangLink'
 import { useTranslation } from 'react-i18next'
 
-const DocumentViewerPerson = ({ doc:person }) => {
+const DocumentViewerPerson = ({ doc }) => {
   const {t} = useTranslation()
-  if (!person) {
+  if (!doc) {
     return null
   }
-  const per = new Person({
-    firstName: person.data.first_name,
-    lastName: person.data.last_name,
-    professions: person.data.professions,
-    birthDate: person.data.birth_date,
-    birthPlace: person.data.birth_place,
-    deathDate: person.data.death_date,
-    deathPlace: person.data.death_place,
-    convoy: person.data.convoy,
-    notes: person.data.notes,
-  })
-  const picture = person.data.resolutions?.thumbnail
+  const per = doc instanceof Person ? doc : Person.create(doc)
+  const picture = per.picture // person.data.resolutions?.thumbnail
   // console.info('DocumentViewerPerson person:', person)
   return (
     <div className="DocumentViewerPerson">
       {picture
-        ? <img src={picture.url} alt={t('pictureOf', { name: person.data.first_name })}/>
+        ? <img src={picture.url} alt={t('pictureOf', { name: per.first_name })}/>
         : null
       }
-      <LangLink to={`/person/${person.slug}`}>
+      <LangLink to={`/person/${per.slug}`}>
       <span className="capitalize font-weight-bold">{per.lastName} <b>{per.firstName}</b></span></LangLink>,
       <span> {per.professions.join(', ')}</span>
        was born in {per.birthPlace} the {per.birthDate}
       <div>convoy: {per.convoy}</div>
+      {/*
       <div className="small p-2">
         {per.notes} [{person.data.households}]
       </div>
+      */}
     </div>
   )
 }
