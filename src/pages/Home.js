843caeb7b7c7f7aeed5e2e21d543eebbbc6f1964
Daniele Guido <gui.daniele@gmail.com>
2021-09-06 15:17:58 +0200

    add text above the landscape

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,24 +3,42 @@ import { Container, Row, Col } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 import { useCurrentWindowDimensions } from '../hooks'
 import StoriesReel from '../components/StoriesReel'
-
+import Landscape from '../components/Landscape'
 
 const Home = () => {
   const { t } = useTranslation()
-  const { height } = useCurrentWindowDimensions()
+  const { width, height } = useCurrentWindowDimensions()
   // load people
   return (
-    <div className="Home mt-5">
-    <Container>
-      <Row>
+    <div className="Home mt-5" >
+    <Container style={{minHeight: height - 10}}>
+      <Row className="my-5">
         <Col>
-          <h1 className="my-5">{t('pagesHomeTitle')}</h1>
-          <p>{t('pagesHomeSubheading')} {t('asNumber', {n: 15040.32456})}</p>
+          <h1>{t('pagesHomeTitle')}</h1>
         </Col>
-
+        <Col>
+          <h2 className="mb-5">{t('pagesHomeSubheading')}</h2>
+          <section dangerouslySetInnerHTML={{__html: t('pagesHomeIntroduction')}} />
+        </Col>
       </Row>
     </Container>
+    <Landscape height={height - 10} width={width}
+      className="position-absolute top-0"
+      style={{ zIndex: -1 }}
+    />
     <StoriesReel height={height}/>
+    <Container>
+      <h3>Lorem ipsum dolor sit amet</h3>
+
+      <Row>
+        <Col>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </Col>
+        <Col>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </Col>
+      </Row>
+    </Container>
     </div>
   )
 }
