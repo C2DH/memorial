3f95d8adea5b8da8f89ce90196bc452bbd08eea2
Daniele Guido <gui.daniele@gmail.com>
2022-09-29 14:10:17 +0200

    fix covers on storyItem component

--- src/components/CoverItems.js
+++ src/components/CoverItems.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import '../styles/components/CoverItems.css'
+
 const CoverItems = ({ covers = [], size = 120, style, ...rest }) => {
+  const pictureStyle = (i) => ({
+    transform: `translate3d(-10px, -50%, 0) rotate(-${i * 3 + Math.random() * 5}deg)`,
+  })
   return (
-    <ol className="CoverItems" style={{ width: size, ...style }} {...rest}>
+    <ol className="CoverItems" style={{ minWidth: size, ...style }} {...rest}>
       {covers.map((doc, i) => {
         const thumbnailResolution = doc.data.resolutions?.thumbnail?.url
         return (
-          <picture key={doc.id}>
+          <picture key={doc.id} style={pictureStyle(i)}>
             <img src={thumbnailResolution} alt={doc.type} style={{ width: size }} />
           </picture>
         )
