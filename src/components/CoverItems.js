43f5d563e66efffd030e5255edd78478705f000a
Daniele Guido <gui.daniele@gmail.com>
2022-09-29 14:46:53 +0200

    add random rotation in Coveritems component

--- src/components/CoverItems.js
+++ src/components/CoverItems.js
@@ -2,9 +2,13 @@ import React from 'react'
 import '../styles/components/CoverItems.css'
 
 const CoverItems = ({ covers = [], size = 120, style, ...rest }) => {
-  const pictureStyle = (i) => ({
-    transform: `translate3d(-10px, -50%, 0) rotate(-${i * 3 + Math.random() * 5}deg)`,
-  })
+  const pictureStyle = (i) => {
+    const neg = i % 2 === 0 ? -1 : 1
+    const theta = i * neg * 5 + Math.random() * neg * 5
+    return {
+      transform: `translate3d(-10px, -50%, 0) rotate(${theta}deg)`,
+    }
+  }
   return (
     <ol className="CoverItems" style={{ minWidth: size, ...style }} {...rest}>
       {covers.map((doc, i) => {
