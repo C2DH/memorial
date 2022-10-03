4f51ff2bff1418a2f63598e27594fcc88e579298
Daniele Guido <gui.daniele@gmail.com>
2022-10-03 12:13:05 +0200

    fix mobile menu

--- src/components/MobileHeader.js
+++ src/components/MobileHeader.js
@@ -4,7 +4,7 @@ import { a, useSpring } from 'react-spring'
 import LanguageSwitch from './LanguageSwitch'
 import LangLink from './LangLink'
 import Logo from './Logo'
-import { HomeRoute, SearchStoryRoute, AboutRoute, TermsOfUseRoute } from '../constants'
+import { HomeRoute, AboutRoute, TermsOfUseRoute, BiographiesRoute } from '../constants'
 import { useStore } from '../store'
 import { useCurrentWindowDimensions } from '../hooks/viewport'
 import '../styles/components/MobileHeader.css'
@@ -29,15 +29,17 @@ const MobileHeader = () => {
     <>
       <header
         className="MobileHeader position-fixed top-0 w-100"
-        onClick={onClick}
         style={{ zIndex: 1000, cursor: 'pointer' }}
       >
-        <div className="MobileHeader_menubtn position-absolute bg-secondary right-0 top-0">
+        <div
+          onClick={onClick}
+          className="MobileHeader_menubtn position-absolute bg-secondary right-0 top-0"
+        >
           <span>{t('Menu')}</span> <Menu size={22} />{' '}
         </div>
         <a.div style={{ width, height, ...style }} className="position-absolute MobileHeader_menu">
           <ul>
-            {[HomeRoute, SearchStoryRoute, AboutRoute, TermsOfUseRoute].map((route) => (
+            {[HomeRoute, BiographiesRoute, AboutRoute, TermsOfUseRoute].map((route) => (
               <li key={route.to} className={route.label === routeLabel ? 'active' : null}>
                 <LangLink to={route.to}>{t(route.label)}</LangLink>
               </li>
@@ -46,12 +48,14 @@ const MobileHeader = () => {
           <LanguageSwitch className="MobileHeader_menu_LanguageSwitch" />
         </a.div>
       </header>
-      <Logo
-        style={{ color: 'white', zIndex: 1001, top: -20, left: -15 }}
-        height={155}
-        width={155}
-        className="MobileHeader_Logo position-absolute mx-2 mt-1"
-      />
+      <LangLink to={HomeRoute.to}>
+        <Logo
+          style={{ color: 'white', zIndex: 1001, top: -20, left: -15 }}
+          height={155}
+          width={155}
+          className="MobileHeader_Logo position-absolute mx-2 mt-1"
+        />
+      </LangLink>
     </>
   )
 }
