2f3c1f276a2a908b9b6472f446e9ef5ff319cb28
donsiamese <donsiamese@gmail.com>
2022-10-06 11:32:21 +0200

    mobile adaptation - people card

--- src/components/PeopleCard.js
+++ src/components/PeopleCard.js
@@ -11,19 +11,22 @@ const PeopleCard = (props) => {
   return (
     <div className="PeopleCard">
       <img src={props.src}></img>
+
       <div className="Wrapper">
-        <h3
-          dangerouslySetInnerHTML={{
-            __html: downsize(selectedPebble.title, { characters: 50, append: '&hellip;' }),
-          }}
-        />
-        <span>Pebble was created on 12.05.2023</span>
-        <p>Jeanne Lukmanski was born in 1920 in Cornellà de Llobregregat (Spain).</p>
-      </div>
-      <div className="Read_More">
-        <b>
-          <VectorIcon></VectorIcon>Read this story
-        </b>
+        <div className="TextWrapper">
+          <h3
+            dangerouslySetInnerHTML={{
+              __html: downsize(selectedPebble.title, { characters: 50, append: '&hellip;' }),
+            }}
+          />
+          <span>Pebble was created on 12.05.2023</span>
+        </div>
+        {/* <p>Jeanne Lukmanski was born in 1920 in Cornellà de Llobregregat (Spain).</p> */}
+        <div className="Read_More">
+          <b>
+            <VectorIcon></VectorIcon>Read this story
+          </b>
+        </div>
       </div>
     </div>
   )
