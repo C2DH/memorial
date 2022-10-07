c60168f40cfa2ae18a6bacd6c8c12bde41e9fc58
Daniele Guido <gui.daniele@gmail.com>
2022-10-07 15:48:00 +0200

    add date parser according to language

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import LangLink from './LangLink'
+import { useDate } from '../hooks/date'
 
 const Person = ({ doc, className = '', active = false }) => {
+  const { t, i18n } = useTranslation()
+  const { parseDate } = useDate({ language: i18n.language.split('_').join('-') })
   return (
     <div className={`Person ${className}`}>
       {active ? (
@@ -12,7 +16,8 @@ const Person = ({ doc, className = '', active = false }) => {
         <h4 className="m-0 fw-bold">{doc.title}</h4>
       )}
       <div className="small">
-        {doc.data.birth_date}
+        {parseDate(doc.data.birth_date)}&nbsp;&mdash;&nbsp;
+        {parseDate(doc.data.death_date, false, null, t('unkownDate'))}
         <br />
         {doc.data.birth_place} {doc.data.birth_country}
       </div>
