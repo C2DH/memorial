be7ef530ffd83a4d886d5b685331e7e1a6840d0b
Daniele Guido <gui.daniele@gmail.com>
2022-12-14 13:52:06 +0100

    limit effects of scrollto top

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -3,32 +3,35 @@ import { useLocation } from 'react-router-dom'
 
 const ScrollToTop = () => {
   const { pathname, hash } = useLocation()
-  console.info('ScrollToTop pathname', pathname, 'hash', hash)
   useEffect(() => {
-    let timer
-    if (hash === '') {
-      window.scrollTo(0, 0)
-      // } else if(hash.indexOf('#p') === 0) {
-      //   return;
-      // } else {
-    } else {
-      timer = setTimeout(() => {
-        // window.dispatchEvent(new Event('resize'))
-        const id = hash.replace('#', '')
-        const element = document.getElementById(id)
-        console.info('ScrollToTop: reaching id =', id)
-        if (element) {
-          element.scrollIntoView()
-          // window.scrollTo(0, element.offsetTop + 100) // window.innerHeight * 0.01)
-        } else {
-          console.warn('ScrollToTop: element not found using id =', id)
-        }
-      }, 500)
-    }
-    return () => {
-      clearTimeout(timer)
-    }
-  }, [pathname, hash])
+    console.debug('[ScrollToTop] \n - pathname:', pathname, ' \n - hash:', hash)
+    window.scrollTo(0, 0)
+  }, [pathname])
+  // useEffect(() => {
+  //   let timer
+  //   if (hash === '') {
+  //     window.scrollTo(0, 0)
+  //     // } else if(hash.indexOf('#p') === 0) {
+  //     //   return;
+  //     // } else {
+  //   } else {
+  //     timer = setTimeout(() => {
+  //       // window.dispatchEvent(new Event('resize'))
+  //       const id = hash.replace('#', '')
+  //       const element = document.getElementById(id)
+  //       console.info('ScrollToTop: reaching id =', id)
+  //       if (element) {
+  //         element.scrollIntoView()
+  //         // window.scrollTo(0, element.offsetTop + 100) // window.innerHeight * 0.01)
+  //       } else {
+  //         console.warn('ScrollToTop: element not found using id =', id)
+  //       }
+  //     }, 500)
+  //   }
+  //   return () => {
+  //     clearTimeout(timer)
+  //   }
+  // }, [pathname, hash])
 
   return null
 }
