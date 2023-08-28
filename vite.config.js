c6bacdcf04b1fe0a97ba2eb0f6ef6d691bf7e92e
Daniele Guido <gui.daniele@gmail.com>
2023-08-28 09:31:22 +0200

    eslint

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  console.log('Proxy', env.VITE_PROXY, 'mode:', mode)
   return {
     build: {
       outDir: 'build',
     },
-    plugins: [react()],
+    plugins: [react(), eslint()],
     loader: { '.js': 'jsx' },
     proxy: {
       '/api': {
-        target: process.env.VITE_PROXY || 'http://localhost',
+        target: env.VITE_PROXY || 'http://localhost',
         changeOrigin: true,
         secure: false,
         ws: true,
       },
       '/media': {
-        target: process.env.VITE_PROXY || 'http://localhost',
+        target: env.VITE_PROXY || 'http://localhost',
         changeOrigin: true,
         secure: false,
         ws: true,
