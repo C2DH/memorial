0c04df4b0a28ee389c3e1e31777dda09022d26f9
Daniele Guido <gui.daniele@gmail.com>
2023-10-03 12:15:14 +0200

    AuthorItem now gets a specific TO param for the link router

--- src/components/AuthorItem.jsx
+++ src/components/AuthorItem.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import LangLink from './LangLink'
 
-const AuthorItem = ({ author, className }) => (
-  <LangLink to={`/biographies?author=${author.slug}`} className={`AuthorItem ${className}`}>
+const AuthorItem = ({ author, className, to = '/biographies?author=:author' }) => (
+  <LangLink to={to.replace(':author', author.slug)} className={`AuthorItem ${className}`}>
     {author.fullname}
   </LangLink>
 )
