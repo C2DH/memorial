aca99f5bfa01bea390fd79484f4f1a66bf63e3a3
Daniele Guido <gui.daniele@gmail.com>
2023-10-05 10:24:20 +0200

    fix gallery

--- src/pages/Slides.jsx
+++ src/pages/Slides.jsx
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom'
-import { useGetJSON } from '../hooks/data'
+import { StatusSuccess, useGetJSON } from '../hooks/data'
 import { useAvailableLanguage } from '../hooks/language'
 import LogoC2dhUnilu from '../components/LogoC2dhUnilu'
 import Logo from '../components/Logo'
@@ -7,15 +7,24 @@ import LogoFondation from '../components/LogoFondation'
 import LogoLePremierConvoi from '../components/LogoLePremierConvoi'
 import { Container, Row, Col } from 'react-bootstrap'
 import './Slides.css'
+import GalleryOfStories from '../components/GalleryOfStories'
 
 const Slides = () => {
   const { pageId } = useParams()
-  const { data: page } = useGetJSON({
+  const { data: page, status: pageStatus } = useGetJSON({
     url: `/api/story/${pageId.replace(/[^\dA-Za-z-_]/g, '')}/`,
   })
-
   const { availableLanguage } = useAvailableLanguage({
     translatable: page?.data?.title,
+  })
+
+  const { data: stories, status: storiesStatus } = useGetJSON({
+    url: `/api/story/`,
+    params: {
+      limit: 50,
+      exclude: { tags__name: 'static' },
+    },
+    enabled: pageStatus === StatusSuccess,
   })
 
   if (!page) return null
@@ -51,29 +60,17 @@ const Slides = () => {
           <span>9.00-17.00</span>
         </h5>
       </div>
-      <div className="Slides__body">
-        <Container fluid className="p-0 my-3">
-          <Row className="Slides__body__row">
-            <Col md={{ span: 4 }} sm={{ span: 12 }}>
-              <div className="Slides__body__image">
-                <img src="/img/lukmanski-carmen.png" alt="alt" />
-              </div>
-            </Col>
-            <Col md={{ span: 8 }} sm={{ span: 12 }}>
-              <div>
-                <h1>
-                  Henriette/Henny SCHEIBERG-UHLMANN (1888-1942), Emil UHLMANN (1879 - 1942), Amalia
-                  SCHEIBERG-OBERMAYER (1895-1944), Siegfried OBERMEYER (1883-1942)
-                </h1>
-                <h5 className="my-4">
-                  WRITTEN BY &nbsp;&nbsp;<span>Maja Veyrat geb. Andert / Denis Scuto</span>
-                </h5>
-                <p>{abstract}</p>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <main className="flex-grow-1">
+        {storiesStatus === StatusSuccess ? (
+          <GalleryOfStories
+            className="h-100"
+            language={availableLanguage}
+            stories={stories.results}
+          />
+        ) : (
+          <div>Loading</div>
+        )}
+      </main>
       <footer>
         <Container fluid className="p-0 my-3">
           <Row className="Slides__body__row">
