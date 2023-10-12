b396f4d1dcdcf70817d78abf2f18cdcf39443214
Daniele Guido <gui.daniele@gmail.com>
2023-10-12 14:43:25 +0200

    refine person page

--- src/components/MetadataField.jsx
+++ src/components/MetadataField.jsx
@@ -1,8 +1,33 @@
-const MetadataField = ({ label, value }) => (
-  <div className="MetadataField">
-    <label>{label}</label>
-    <span>{value}</span>
-  </div>
-)
+import './MetadataField.css'
+import { useTranslation } from 'react-i18next'
+
+const MetadataField = ({ label, value, className = '', hideIfEmpty = false }) => {
+  var { t } = useTranslation()
+
+  if (
+    hideIfEmpty &&
+    (typeof value === 'undefined' ||
+      value === null ||
+      (typeof value === 'string' && value.length === 0))
+  ) {
+    return null
+  }
+  const valueAsDate = new Date(value)
+
+  return (
+    <div className={`MetadataField ${className}`}>
+      <label>{t('metadataField_' + label)}</label>
+      {typeof value === 'undefined' || value === null ? <span>&mdash;</span> : null}
+      {typeof value === 'string' && value.length === 0 ? <span>&mdash;</span> : null}
+      {typeof value === 'string' && isNaN(valueAsDate) && value.length > 0 ? (
+        <span>{value}</span>
+      ) : null}
+      {typeof value === 'string' && !isNaN(valueAsDate) ? (
+        <span>{t('dateShort', { date: valueAsDate })}</span>
+      ) : null}
+      {Array.isArray(value) ? value.map((v, i) => <span key={i}>{v}</span>) : null}
+    </div>
+  )
+}
 
 export default MetadataField
