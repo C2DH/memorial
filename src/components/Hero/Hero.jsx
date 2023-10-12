8cdd7f616dfcaab6e29b700b92d6afe12f310e59
Daniele Guido <gui.daniele@gmail.com>
2023-10-12 19:02:24 +0200

    load stories in Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -13,13 +13,38 @@ import { StatsGl, Stats } from '@react-three/drei'
 
 import { useEffect } from 'react'
 import { usePebblesStore } from './store'
+import { StatusSuccess, useGetJSON } from '../../hooks/data'
 
-const Hero = ({ isMobile }) => {
-  useEffect(() => {
-    if (usePebblesStore.getState().pebblesData.length > 0) return
-    usePebblesStore.getState().setInitialData()
+const Hero = ({ isMobile, n = 100 }) => {
+  // preload initial dataset
+  const { data, status, error } = useGetJSON({
+    url: '/api/story',
+    params: {
+      limit: 100,
+      exclude: {
+        tags__slug__in: ['static', 'convoy'],
+      },
+      // filters: {
+      //   slug__in: [
+      //     '0076-grossvogel-macharowska',
+      //     '0109-abraham-fredy',
+      //     '0127-dorflaufer',
+      //     '0010-adler-wolf-ermann',
+      //     '0014-esther-meyer',
+      //   ],
+      // },
+      // orderby: '-date_last_modified',
+    },
   })
 
+  useEffect(() => {
+    if (status !== StatusSuccess) return
+    if (usePebblesStore.getState().pebblesData.length > 0) return
+    usePebblesStore.getState().setInitialData()
+  }, [status])
+  if (error) {
+    console.error('[Hero] useGetJSON', error)
+  }
   return (
     <div className="hero">
       <div className="hero__canvas-wrapper">
@@ -32,8 +57,8 @@ const Hero = ({ isMobile }) => {
       </div>
       <Overlay isMobile={isMobile} />
       <div className="hero__modals">
-        <ModalDetails />
-        <ModalCreate />
+        <ModalDetails stories={data?.results} />
+        <ModalCreate stories={data?.results} />
       </div>
     </div>
   )
