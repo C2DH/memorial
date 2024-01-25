ce86bf5f50654a3275f0596222c0290cc83d0aab
donsiamese <donsiamese@gmail.com>
2024-01-25 11:44:26 +0100

    fixes for header mobile/desktop versions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,55 +25,61 @@ const Header = () => {
   const isRootPath = window.location.pathname.match(LanguageRootPathRegExp)
   console.debug('[Header] isRootPath:', isRootPath, '\n - routeLabel:', routeLabel)
   return (
-    <header
-      className="Header"
-      style={{ padding: routeLabel !== HomeRoute.label ? '1rem 0' : null }}
-    >
+    <>
       {!isMobile && routeLabel === HomeRoute.label ? (
         <div className="overlay__logo">
-          <LogoMemorial width={160} />
+          <LogoMemorial width={180} />
         </div>
       ) : null}
-      <div className="home-nav-wrapper">
-        {routeLabel !== HomeRoute.label ? (
-          <>
-            <LangLink to={HomeRoute.to} className="">
-              <LogoMemorialCompact width={100} className="me-3" color="var(--bs-primary-text)" />
-            </LangLink>
-            <div className={routeLabel === HomeRoute.label ? 'active' : null}>
+      <header className="Header">
+        <div className="container p-0 d-flex justify-content-between">
+          <div className="home-nav-wrapper">
+            {routeLabel !== HomeRoute.label ? (
+              <>
+                <LogoMemorialCompact
+                  width={100}
+                  className="me-3 position-absolute"
+                  color="var(--bs-primary-text)"
+                />
+              </>
+            ) : null}
+            <div
+              style={{ paddingLeft: '8rem' }}
+              className={routeLabel === HomeRoute.label ? 'active' : null}
+            >
               <LangLink to={HomeRoute.to} className="">
                 {t(HomeRoute.label)}
               </LangLink>
             </div>
-          </>
-        ) : null}
-      </div>
+          </div>
 
-      <nav className="Header_Nav" role="navigation" aria-label={t('ariaLabelLanguageAbout')}>
-        <ul className="d-flex justify-content-end">
-          <li
-            className={
-              [BiographiesRoute.label, StoryRoute.label].includes(routeLabel) ? 'active' : null
-            }
-          >
-            <LangLink to={BiographiesRoute.to}>{t(BiographiesRoute.label)}</LangLink>
-          </li>
-          <li className={routeLabel === AboutRoute.label ? 'active' : null}>
-            <LangLink to={AboutRoute.to} className="">
-              {t(AboutRoute.label)}
-            </LangLink>
-          </li>
-          <li className={routeLabel === FaqRoute.label ? 'active' : null}>
-            <LangLink to={FaqRoute.to} className="">
-              {t(FaqRoute.label)}
-            </LangLink>
-          </li>
-          <li>
-            <LanguageSwitch isRootPath={isRootPath} className="d-flex" linkClassName="ms-3" />
-          </li>
-        </ul>
-      </nav>
-    </header>
+          <nav className="Header_Nav" role="navigation" aria-label={t('ariaLabelLanguageAbout')}>
+            <ul className="d-flex justify-content-end">
+              <li
+                className={
+                  [BiographiesRoute.label, StoryRoute.label].includes(routeLabel) ? 'active' : null
+                }
+              >
+                <LangLink to={BiographiesRoute.to}>{t(BiographiesRoute.label)}</LangLink>
+              </li>
+              <li className={routeLabel === AboutRoute.label ? 'active' : null}>
+                <LangLink to={AboutRoute.to} className="">
+                  {t(AboutRoute.label)}
+                </LangLink>
+              </li>
+              <li className={routeLabel === FaqRoute.label ? 'active' : null}>
+                <LangLink to={FaqRoute.to} className="">
+                  {t(FaqRoute.label)}
+                </LangLink>
+              </li>
+              <li className="pe-2">
+                <LanguageSwitch isRootPath={isRootPath} className="d-flex" linkClassName="ms-3" />
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
   )
 }
 
